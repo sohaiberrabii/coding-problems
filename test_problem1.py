df062d47ac8a2c510c2e62b02aeb7e12a7eac879
Sohaib Errabii <errabii.sohaib@gmail.com>
2022-06-04 12:19:59 +0200

    Add test cases

--- test_problem1.py
+++ test_problem1.py
@@ -1,2 +1,17 @@
-def test_solution():
-    assert False
+import pytest
+
+from problem1 import solution
+
+
+@pytest.mark.parametrize(
+    "x, y, expected",
+    [
+        ([13, 5, 6, 2, 5], [5, 2, 5, 13], 6),
+        ([14, 27, 1, 4, 2, 50, 3, 1],
+         [2, 4, -4, 3, 1, 1, 14, 27, 50], -4)
+    ]
+)
+def test_solution(x, y, expected):
+    assert solution(x, y) == expected
+
+
